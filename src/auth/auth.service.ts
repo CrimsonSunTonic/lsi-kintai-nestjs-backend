cdccea07a5518ca1ccf860c7fffc6e45462d3a8f
nhatcomnhom <nhatcomnhom@gmail.com>
2025-10-03 13:00:17 +0900

    add base signup logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { signUpDto } from './dto';
+import * as argon2 from 'argon2';
 
 @Injectable()
 export class AuthService {
     constructor(private prisma:PrismaService) {}
     // signup
-    signup() {
-        return { msg: 'signup' };
+    async signup(dto: signUpDto) {
+        console.log(dto);
+        // Generate the password hash
+        const hash = await argon2.hash(dto.password);
+        //Save the new user in the db
+        const user = await this.prisma.user.create({
+            data: {
+                email: dto.email,
+                password: hash,
+            },
+        });
+
+        console.log(user);
+        const { password, ...userWithoutPassword } = user;
+
+        //Return the saved user
+        return userWithoutPassword;
     }
 
     // signin
---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,13 @@
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
+export class signUpDto {
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    password: string;
+    firstname: any;
+    lastname: any;
+}
