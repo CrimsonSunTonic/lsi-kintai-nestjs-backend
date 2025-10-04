ab51f04bf2509dba3c1cd199650ec104e6288680
nhatcomnhom <nhatcomnhom@gmail.com>
2025-10-04 10:03:39 +0900

    set read .env config

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-    constructor() {
+    constructor(config: ConfigService) {
         super({
             datasources: {
                 db: {
-                    url: 'postgresql://devuser:devpass@localhost:5434/devdb',
+                    url: config.get('DATABASE_URL'),
                 },
             },
         });
-    }
+    }   
 }
