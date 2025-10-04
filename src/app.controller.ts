34e435f242a638278704abfbe1172b4f6c91f1fd
nhatcomnhom <nhatcomnhom@gmail.com>
2025-10-04 11:36:12 +0900

    add jwt guard logic

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import type { Request as ExpressRequest } from 'express';
+import { JwtGuard } from './auth/guard';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,11 @@ export class AppController {
   @Get('hello')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @UseGuards(JwtGuard)
+  @Get('me')
+  getMe(@Req() req: ExpressRequest ) {
+    return req.user;
   }
 }
