0b5c73e180be0797591d0032789beaa2bbce2443
nhatcomnhom <nhatcomnhom@gmail.com>
2025-10-04 16:10:53 +0900

    custome decorator for get user request

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,5 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import type { Request as ExpressRequest } from 'express';
-import { JwtGuard } from './auth/guard';
 
 @Controller()
 export class AppController {
@@ -12,9 +10,4 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @UseGuards(JwtGuard)
-  @Get('me')
-  getMe(@Req() req: ExpressRequest ) {
-    return req.user;
-  }
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller,  Get,  Req, UseGuards } from '@nestjs/common';
+import { JwtGuard } from '../auth/guard';
+import { GetUser } from '../auth/decorator';
+import type { User } from 'generated/prisma';
 
+@UseGuards(JwtGuard)
 @Controller('user')
-export class UserController {}
+export class UserController {
+
+  @Get('me')
+  getMe(@GetUser() user: User, @GetUser('email') email: string) {
+    console.log(email);
+    return user;
+  }
+}
