0801e571dc0db42d128785f9ae2477b9168d4191
nhatcomnhom <nhatcomnhom@gmail.com>
2025-10-07 23:15:36 +0900

    add find all user endpoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,13 @@
-import { Controller,  Get,  Req, UseGuards } from '@nestjs/common';
+import { Controller,  ForbiddenException,  Get,  Req, UseGuards } from '@nestjs/common';
 import { JwtGuard } from '../auth/guard';
 import { GetUser } from '../auth/decorator';
 import type { User } from '@prisma/client';
+import { UserService } from './user.service';
 
 @UseGuards(JwtGuard)
 @Controller('user')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
 
   @Get('me')
   getMe(@GetUser() user: User) {
@@ -16,5 +18,14 @@ export class UserController {
       lastname: user.lastname,
       role: user.role,
     };
+  }
+
+  @Get('all')
+  async getAllUsers(@GetUser() user: User) {
+    if (user.role !== 'ADMIN') {
+      throw new ForbiddenException('Access denied: Admins only');
+    }
+
+    return this.userService.getAllUsers();
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  // ✅ Return all users except admin
+  async getAllUsers() {
+    return this.prisma.user.findMany({
+      where: {
+        role: {
+          not: 'ADMIN',
+        },
+      },
+      select: {
+        id: true,
+        email: true,
+        firstname: true,
+        lastname: true,
+        role: true,
+        createdAt: true,
+      },
+      orderBy: { id: 'asc' },
+    });
+  }
+}
